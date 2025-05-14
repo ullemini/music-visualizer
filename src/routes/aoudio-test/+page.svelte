03782143ed3212fd9e47638b9155a8bd0ee8cd05
ullemini <ullesone@gmail.com>
2025-05-14 14:35:18 +0200

    refactor + beginig of settings

--- src/routes/aoudio-test/+page.svelte
+++ src/routes/aoudio-test/+page.svelte
@@ -1,29 +1,35 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import { Segment } from '@skeletonlabs/skeleton-svelte'; 
 
-    let canvasDOM:HTMLCanvasElement
+    const audioContext = new AudioContext();
+    let canvasDOM:HTMLCanvasElement;
+    let audioElement:HTMLAudioElement;
+    let analyser:AnalyserNode = audioContext.createAnalyser();
+    let canvasContext:CanvasRenderingContext2D ;
+    let frequencydata:Uint8Array<ArrayBuffer>
     const VisulizerStyle = {
-        Bar:"bar",
+        bar:"bar",
         off:"off",
-        Other:"other"
-
+        beta:"beta",
+        other:"other"
     };
-    let currentVisulizerStyle:string = "bar"
-
+    const fftSize = {
+        default:"256",
+        dubble:"512"
+    }
+    let currentVisulizerStyle:string = $state("bar")
+    let currentfftSize:string = $state(`${fftSize.default}`)
     onMount(()=>{ 
         
-        const audioContext = new AudioContext();
-        const audioElement = document.getElementById("mainAudio");
-        const canvas = document.getElementById("maincanvas");
-        const canvasContext = canvas.getContext("2d");
-        const analyser = audioContext.createAnalyser();
+        canvasContext = canvasDOM.getContext("2d")
         analyser.fftSize = 256
         const bufferLength = analyser.frequencyBinCount;
         
         let aoudioSorce = audioContext.createMediaElementSource(audioElement);
         aoudioSorce.connect(analyser)
         analyser.connect(audioContext.destination);
-        const frequencydata = new Uint8Array(bufferLength);
+        frequencydata = new Uint8Array(bufferLength);
         analyser.smoothingTimeConstant = 0.65;
         
         function draw(){
@@ -35,18 +41,28 @@
 
             canvasContext.clearRect(0, 0, canvas_width, canvas_height);
             switch (currentVisulizerStyle){
-                case "off": break; 
-                case "bar": style1(canvas_width,canvas_height); break;
+                case VisulizerStyle.off: break; 
+                case VisulizerStyle.beta:
+                    canvasContext.fillStyle = `rgb(255 255 255)`; 
+                    canvasContext.fillRect(0, 0, canvas_width,canvas_height); 
+                    break;
+                case VisulizerStyle.bar: 
+                    visulizerStyleBar(canvas_width,canvas_height,bufferLength,frequencydata); 
+                    break;
                 
             }
         }
-        function style1(width,height){
+        
+       draw();
+
+    })
+    
+    function visulizerStyleBar(width:number,height:number,bufferLength:number,frequencydata:Uint8Array<ArrayBuffer>){
             analyser.getByteFrequencyData(frequencydata);
             const barWidth = (width / bufferLength) * 2.5;
             let barHeight;
             let x = 0;
             canvasContext.fillStyle = `rgb(255 250 250)`; 
-            canvasContext.clearRect(0, 0, height, width);
             for (let i = 0; i < bufferLength; i++) {
                 barHeight = 1 + (frequencydata[i] / 2);
                 canvasContext.fillStyle = `rgb(${barHeight + 100} ${Math.floor(x/2)}  ${i*8})`; 
@@ -55,24 +71,35 @@
                 x += barWidth + 1;
             }
         }
-       draw();
-
-    })
-
-    function clickybutton(){
-        
-    }
-    
+        function changeffsise(zise:string) {
+            switch (zise){
+                case "256": analyser.fftSize = 256; break;
+                case "512": analyser.fftSize = 512; break;
+            }
+        }
 </script>
 
-<main>
-    <div id=controll-pannel >
-        <button id=button1 on:click={()=>{clickybutton}} >button 1</button>
-        <audio src="aoudio\wise-mystical-magical-wizard.mp3" id="mainAudio" controls loop></audio>
+<main >
+    <div id="controll-pannel" >
+        <Segment value={currentVisulizerStyle} onValueChange={(e) => (currentVisulizerStyle = e.value)}>
+            <Segment.Item value="bar">Bar</Segment.Item>
+            <Segment.Item value="beta">Beta</Segment.Item>
+            <Segment.Item value="off">Off</Segment.Item>
+        </Segment >
+
+        <Segment value={currentfftSize} onValueChange={(e) => (changeffsise(e.value))}>
+            {#each Object.entries(fftSize) as [name,sise]}
+                <Segment.Item value={`${sise}`}>
+                    {name} {sise}
+                </Segment.Item>
+            {/each}
+            
+        </Segment>
     </div>
-    <div id=visulizer-div >
-        <canvas id=maincanvas width="1800" height="150" bind:this={canvasDOM}><p>visulizer</p></canvas>
+    <div id="visulizer-div" >
+        <canvas id="maincanvas" width="1800" height="150" bind:this={canvasDOM}><p>visulizer</p></canvas>
     </div>
+    <audio src="aoudio\wise-mystical-magical-wizard.mp3" id="mainAudio" controls loop bind:this={audioElement}></audio>
 </main>
 
 <style>
@@ -83,13 +110,14 @@
     canvas{
         width: 100%;
         height: 100%;
-        border-width: 2px;   
-        border-color: brown;
+    }
+    audio{
+        width: 100vw;
     }
     #visulizer-div{
         width: 100vw;
         height: 80vh;
-        border-width: 1px;   
+       
     }
     #controll-pannel{
         display: flex;
